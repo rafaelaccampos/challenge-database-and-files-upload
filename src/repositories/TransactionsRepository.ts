a981307848667ade887fe684a18db630eedafd7e
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-08-21 20:43:20 -0300

    feat: Challenged accepted

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,27 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator: Balance, transactions) => {
+        if (transactions.type === 'income') {
+          accumulator.income += Number(transactions.value);
+        } else {
+          accumulator.outcome += Number(transactions.value);
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
